855bf681e9db4db8da5aaad0cadadd6ac833bb7e
Jenny <156132099+Jennyznz@users.noreply.github.com>
2026-01-11 23:45:40 -0500

    Debug game setup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 import { Game } from './game';
 import './styles.css';
 
-UISetup();
 const game = new Game();
+UISetup(game);
 
-function UISetup() {
+
+
+function UISetup(game) {
     const gridOne = document.querySelector('.grid-one');
     const gridOneRows = gridOne.querySelectorAll('.row');
 
     // Add coordinate points to each cell in the grid
-    gridOneRows.forEach(row, r => {
+    gridOneRows.forEach((row, r) => {
         const cells = row.querySelectorAll('.cell');
-        cells.forEach(cell, c => {
+        cells.forEach((cell, c) => {
             cell.dataset.row = r;
             cell.dataset.column = c;
         });
     });
 
     // Add event listeners to player one (human/"real") grid
-    const gridOneCells = gridOneRows.querySelectorAll('.cell');
+    const gridOneCells = gridOne.querySelectorAll('.cell');
     gridOneCells.forEach(cell => {
-        cell.attachEventListeners('click', () => {
-            game.playerOneMove(cell.dataset.row, cell.dataset.column);
+        cell.addEventListener('click', () => {
+            const row = Number(cell.dataset.row);
+            const column = Number(cell.dataset.column);
+
+            game.playerOneMove(row, column);
+            console.log(row, column);
         });
     });
 }
